5602ead611c571bbc52cd26f9ce695825bc08d11
zhangke-zhangke <Ke18662761575@163.com>
2025-09-19 12:30:00 +0800

    装饰器运行顺序解析

--- python_decorator.py
+++ python_decorator.py
@@ -1,6 +1,6 @@
 '''
 python装饰器
-通过@xxx语法糖方式，附加到函数头上，调用函数时，会将函数作为参数调用装饰器函数。
+通过@xxx语法糖方式，附加到函数头上，调用函数时，会将函数作为参数调用装饰器函数。支持带参数的装饰器，内部逻辑分为构建时、执行时。装饰器在函数构建时完成
 其中在函数执行前后一些操作，如：日志记录、参数校验等。常用于以下场景。
     1、业务功能扩展
     2、日志记录、参数校验
@@ -22,26 +22,31 @@ class DefineDecorator:
         print(level)
         def decorator(fun: Callable):
             def wrapper(*args, **kwargs):
-                print('记录开始')
-                print(f'在xxx时间xxx秒，调用了{fun.__name__}函数')
+                print('recordLog记录开始')
+                print(f'recordLog在xxx时间xxx秒，调用了{fun.__name__}函数')
                 result = fun(*args, **kwargs)
-                print('记录结束')
+                print('recordLog记录结束')
                 return result
             return wrapper
         return decorator
 
     @staticmethod
     def checkType(tp):
+        print(tp)
         def decorator(fun):
             def wrapper(*args, **kwargs):
                 for arg in args:
                     if not isinstance(arg, tp):
                         raise TypeError(f'{arg}类型错误')
+                else:
+                    print('类型校验通过')
+                print(f'checkType在xxx时间xxx秒，调用了{fun.__name__}函数')
                 return fun(*args, **kwargs)
             return wrapper
         return decorator
 
 
+# checkType(recordLog(calc))
 @DefineDecorator.checkType(int)
 @DefineDecorator.recordLog(level='info')
 def calc(a: int, b: int) -> int:
@@ -54,9 +59,11 @@ def calc(a: int, b: int) -> int:
     print('开始计算')
     return a + b
 
+print('-----------------')
+r = calc(3, 4)
+print(r)
 
-# r = calc(3, 4)
-# print(r)
+
 import math
 
 def printV(v):
@@ -67,11 +74,11 @@ lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 powLista = list(map(lambda x: printV(x), lista))
 f50Lista = list(filter(lambda v: v >= 50, powLista))
 reduceLista = reduce(lambda x, y: x + y, f50Lista)
-print(reduceLista)
+# print(reduceLista)
 
-
-if (l:= reduceLista) > 244:
-    print('reduceLista大于144')
+# # py >= 3.8 特有语法
+# if (l:= reduceLista) > 244:
+#     print('reduceLista大于144')
 
 
 
